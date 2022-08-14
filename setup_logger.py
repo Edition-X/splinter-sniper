c52ade794bff4c181890ce4a52a71486e2a471d5
Daniel Kelly <danielkelly89@googlemail.com>
2022-08-14 16:25:36 +0200

    Logging and docker-compose (#2)
    * Add logs dir but not *.log files
    
    * Export logs from docker to host machine and add docker-compose support

--- setup_logger.py
+++ setup_logger.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 if "DEBUG" in os.environ:
     logger.setLevel(logging.DEBUG)
-filename = (f'transactions-{datetime.now():%Y-%m-%w}.log')
+filename = (f'logs/transactions-{datetime.now():%Y-%m-%w}.log')
 formatter = logging.Formatter("%(asctime)s - %(message)s", "%Y-%m-%d:%H-%M-%S")
 fileHandler = TimedRotatingFileHandler(os.path.join(os.path.dirname(os.path.abspath(__file__)), filename),  when='midnight')
 fileHandler.setFormatter(formatter)
